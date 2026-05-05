7360e9cf260a8b108dd7605832b2c7c44401a036
Vaynah <154355759+guanhua202@users.noreply.github.com>
2026-05-05 15:38:26 +0300

    Add files via upload

---
+++ search_word.py
@@ -0,0 +1,23 @@
+# Beta v0.1
+
+file_names = ['file1.txt', 'file2.txt', 'file3.txt']
+search_word = str(input("Слово которое вы ищете: "))
+
+for file_name in file_names:
+
+    with open(file_name) as file:
+        
+        count_string = 0
+
+        for line in file.readlines():
+            line = line.strip().split()
+            count_string += 1
+
+            for word in line:
+                if word == search_word:
+                    print(f"Слово {search_word} находится в файле {file_name} на строке №{count_string}")
+
+
+# Beta v0.2
+
+# 
